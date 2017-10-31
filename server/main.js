4a6845849ce75ec1ef8d36cae85c04b7d266b4b4
Yorrd <yorrd.adornis@gmail.com>
2017-10-31 12:48:38 +0100

    more shit

--- server/main.js
+++ server/main.js
@@ -1,8 +1,17 @@
 import { Meteor } from 'meteor/meteor';
+// import * as data from '../imports/jsonFile.js';
+
+// const fs = require('fs');
 
 const TestCollection = new Mongo.Collection('mongo-data');
+const SpendingsCollection = new Mongo.Collection('spendings');
 
 Meteor.startup(() => {
     // code to run on server at startup
     Meteor.publish('mongo-data');
+    Meteor.publish('spendings');
+
+    // read initial data
+    // console.log(data);
+    // console.log(data.length);
 });
