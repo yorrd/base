8286e08c920d1cb073d90ee66ea608cbd33a3c3e
Yorrd <yorrd.adornis@gmail.com>
2017-11-12 16:15:58 +0100

    first try, waiting for upstream

--- server/main.js
+++ server/main.js
@@ -1,6 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { onPageLoad } from 'meteor/server-render';
 
+// import { parseHtml, handleTags } from 'meteor/mwc:synthesis-compiler';
+
+const render = require('@skatejs/ssr');
+
 // import * as data from '../imports/jsonFile.js';
 
 // const fs = require('fs');
@@ -38,6 +42,18 @@ Meteor.startup(() => {
     // console.log(data.length);
 });
 
-// onPageLoad((sink) => {
-//     sink.appendToBody('<test-layout></test-layout>');
-// });
+class Hello extends HTMLElement {
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.innerHTML = '<span>Hello!</span>';
+    }
+}
+render(new Hello(), true);
+
+onPageLoad((sink) => {
+    // import('../imports/ui/components/test-layout.js').then(layout => {
+    //     render(new layout()).then(console.log);
+    // });
+    sink.appendToBody('<div id="app"></div>afaf');
+    // sink.appendToBody('<test-layout></test-layout><dom-module id="test-layout">\n  <template>\n    <style is="custom-style" include="iron-flex iron-flex-alignment adornis"></style>\n    <style>:root{font-family:sans-serif}.main{@apply(--layout-horizontal);@apply(--layout-center-center);}app-header{background-color:var(--primary-color);color:#fff;}app-header paper-icon-button{--paper-icon-button-ink-color:white;}.print-break{page-break-after:always;page-break-inside:avoid;}paper-input-container input{@apply(--paper-input-container-shared-input-style);}hr{margin:3em 0;}</style>\n\n    \n\n    <app-location route="{{route}}"></app-location>\n\n    <app-route route="{{route}}" pattern="/:page" data="{{routeData}}"></app-route>\n\n    \n\n    <app-header-layout class="fit layout vertical">\n\n        <mongo-repeat id="mdata" collection="mongo-data" sub-params="{{subParams}}" debounce-interval="100">\n            <template>\n                <paper-card>{{item.name}}</paper-card>\n            </template>\n        </mongo-repeat>\n\n        <paper-toggle-button on-checked-changed="_toggleFilter"></paper-toggle-button>\n        <paper-button on-tap="_add">add</paper-button>\n\n        <hr>\n\n        <paper-button on-click="handleUpdateMessage">Insert Hallo Welt</paper-button>\n        <mongo-data id="mongoData" filter="{{filter}}"></mongo-data>\n\n        <hr>\n\n        \n                    \n                    \n                \n                \n\n    </app-header-layout>\n\n  </template>\n</dom-module>\n\n\n');
+});
