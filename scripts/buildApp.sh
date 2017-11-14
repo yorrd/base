7a911e3145576a79c70fdca8718643e2543750e1
Elias Künstler <elias@adornis.de>
2017-11-14 13:07:31 +0100

    collection persistency and scripts

--- scripts/buildApp.sh
+++ scripts/buildApp.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 isrun=$1
+server=$2
 
 export ANDROID_HOME=/opt/android-sdk
-export ROOT_URL=http://192.168.0.102:3000/
+export ROOT_URL=$server
 
 echo "Building Application..."
-    meteor build ../qrcodeextension_build --server http://192.168.0.102:3000/
-echo "Finished Building!"
+    meteor build ../qrcodeextension_build --server $server
+
 echo "Signing Application..."
     cd ../qrcodeextension_build/android
     jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 release-unsigned.apk elearningapp
-echo "Finished Signing!"
+
 echo "Zipalign Application..."
     /opt/android-sdk/build-tools/25.0.3/zipalign 4 release-unsigned.apk elearning_qr.apk
+
 echo "Finished Application!"
 
 if [ "$isrun" == "run" ] ; then
