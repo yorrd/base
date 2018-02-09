cf455d473cc743054631db36a663fd5ab409606f
Elias Künstler <elias@adornis.de>
2018-02-09 15:36:16 +0100

    updated upload.sh to remind of versino changes

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -3,5 +3,11 @@ projectname=$1
 sshaccess=$2
 sshport=$3
 
+echo "Did you update the version number in all of the following files?"
+echo "ServiceWorker - sw.js"
+echo "package.json"
+echo "mobile-config.js"
+read -p "Press Enter to continue"
+
 meteor build ./ --server-only --architecture os.linux.x86_64
  scp -P $sshport $projectname.tar.gz $sshaccess
